24c55275024b8a2273985b48ec10f91d8bea3f19
Phillip Pimenta <phillip@phpimenta.com.br>
2020-03-01 13:50:17 -0300

    Correção no componente de collapse da versão 0.1.7

--- src/js/phpimenta-ui.js
+++ src/js/phpimenta-ui.js
@@ -1,26 +1,28 @@
 /**
  * --------------------------------------------------------------------------
- * Phpimenta UI Framework (v0.1.7)
+ * Phpimenta UI Framework (v0.1.8)
  * Copyright 2020-2020 Phpimenta Software e Consultoria
  * Licensed under MIT (https://github.com/phpimenta/phpimenta-ui-framework/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
 
-let cards = document.querySelectorAll(
-  ".phpimenta-ui-card-collapse .phpimenta-ui-card"
-);
-let header;
-let body;
-cards.forEach(card => {
-  Array.from(card.children).forEach(element => {
-    if (element.classList.value.match(/card-header/i)) {
-      header = element;
-    } else if (element.classList.value.match(/card-body/i)) {
-      body = element;
-    }
-  });
-  header.addEventListener("click", function (event) {
-    card.classList.toggle("show");
+window.addEventListener('load', function () {
+  let cards = document.querySelectorAll(
+    ".phpimenta-ui-card-collapse .phpimenta-ui-card"
+  );
+  let header;
+  let body;
+  cards.forEach(card => {
+    Array.from(card.children).forEach(element => {
+      if (element.classList.value.match(/phpimenta-ui-card-header/i)) {
+        header = element;
+      } else if (element.classList.value.match(/phpimenta-ui-card-body/i)) {
+        body = element;
+      }
+    });
+    header.addEventListener("click", function (event) {
+      card.classList.toggle("show");
+    });
   });
 });
 
