3547aaca6ab275e8527f0ed9e4fccf2581cbc22d
Phillip Pimenta <phillip@phpimenta.com.br>
2020-03-01 21:17:39 -0300

    Correções no sistema de grid

--- dist/js/phpimenta-ui.js
+++ dist/js/phpimenta-ui.js
@@ -8,7 +8,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 /**
  * --------------------------------------------------------------------------
- * Phpimenta UI Framework (v0.1.9)
+ * Phpimenta UI Framework (v0.2.0)
  * Copyright 2020-2020 Phpimenta Software e Consultoria
  * Licensed under MIT (https://github.com/phpimenta/phpimenta-ui-framework/blob/master/LICENSE)
  * --------------------------------------------------------------------------
--- src/js/phpimenta-ui.js
+++ src/js/phpimenta-ui.js
@@ -1,6 +1,6 @@
 /**
  * --------------------------------------------------------------------------
- * Phpimenta UI Framework (v0.1.9)
+ * Phpimenta UI Framework (v0.2.0)
  * Copyright 2020-2020 Phpimenta Software e Consultoria
  * Licensed under MIT (https://github.com/phpimenta/phpimenta-ui-framework/blob/master/LICENSE)
  * --------------------------------------------------------------------------
