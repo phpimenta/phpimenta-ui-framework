fce8efc1d7efbc46a613e004042213f7da315b17
Phillip Pimenta <phillip@phpimenta.com.br>
2020-03-31 17:30:40 -0300

    Arquivos da v0.2.4

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,5 @@
 /*!
-* Phpimenta UI Framework v0.2.3
+* Phpimenta UI Framework v0.2.4
 * Copyright 2020-2020 Phpimenta Software e Consultoria
 * Licensed under MIT (https://github.com/phpimenta/phpimenta-ui-framework/blob/master/LICENSE)
 */
